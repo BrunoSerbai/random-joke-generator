c67e60ee21f01daa45d5cdf2fc2aa0063799d48d
Bruno Serbai <brunoserbai@outlook.com>
2025-01-24 03:14:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-yellow-300 w-screen h-screen justify-center .flex">
+      <div className="bg-black clr-white rounded-2xl w-3/5">
+        <img></img>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. ciunt
+          voluptates ipsa quidem! Perferendis debitis nostrum iure in.
+        </p>
+        <button></button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
