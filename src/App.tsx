8653130b51b42707354cbf4529781676b3c88872
Bruno Serbai <brunoserbai@outlook.com>
2025-01-28 00:17:22 -0300

    add responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,8 @@ function App() {
   return (
     <div className="bg-yellow-300 flex items-center justify-center h-screen">
       <div className="bg-black rounded-2xl w-3/5 p-5 text-center shadow flex flex-col justify-center items-center">
-        <img className="laught-img h-50 m-5" src="/laught.png" alt="example" />
-        <p className="text-white text-base mt-10 mb-5 w-max break-words ">{joke}</p>
+        <img className="laught-img max-h-60 m-5" src="/laught.png" alt="example" />
+        <p className="text-white text-base mt-10 mb-5  break-all ">{joke}</p>
         <button id="joke" className="bg-yellow-300 h-8 px-4 rounded m-5">
           Get Joke
         </button>
